a84156acdc6ced45db69936bddc379116af4ccd8
Uriel thoma <108682061+UrielCT@users.noreply.github.com>
2023-08-04 13:58:04 -0300

    Create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='app-layout'>
+
+      <header>
+        <h1>MyTinerary</h1>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Cities</a>
+        </nav>
+        <button>
+          <div>
+            <img src="" alt="person" />
+            <p>Login</p>
+          </div>
+        </button>
+      </header>
+
+      <main>
+        <>
+          <div>
+            <h2>Find your perfect trip, designed by insiders who know and love their cities!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quia, molestias nostrum 
+              quod optio ipsum possimus esse fuga deserunt ratione quasi, earum cupiditate voluptas aliquam!
+               Deserunt enim cumque atque recusandae!</p>
+            <button>View More</button>
+          </div>
+          <img src="" alt="imagen_inicio" />
+        </>
+        
+      </main>
+      
+      <footer>
+        <p>Todos los derechos reservados. Uriel Thoma 2023</p>
+      </footer>
+
+    </div>
+  )
+}
